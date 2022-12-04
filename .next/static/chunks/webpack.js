8e8a8e87016b2d7fac2165e0fe7647b821c2c10e
pablo <pablogabriel.devweb@gmail.com>
2022-12-04 20:44:28 -0300

    primeira pagina

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c77d224aa8e17b16"; }
+/******/ 		__webpack_require__.h = function() { return "0ad7ae493f330f60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
