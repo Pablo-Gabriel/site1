60b439d84f089fdf72120bf8d4510b77b8d91a73
pablo <pablogabriel.devweb@gmail.com>
2022-12-04 20:59:22 -0300

    codigo limpo

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,6 @@
-import { useState } from 'react';
-
 function Home() {
     return <div>
             <h1>Home</h1>
-            <Contador />
         </div>
 }
-
-function Contador(){
-    const [contador, setcontador] = useState(1);
-
-    function adicionarContador() {
-        setcontador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-
-}
-
-
-
-
 export default Home
